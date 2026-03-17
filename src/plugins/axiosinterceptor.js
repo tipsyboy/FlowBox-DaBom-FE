d48720c0ccbf20c4afd741b0e73e6b4c973a0e3c
tipsyboy <gudah13@naver.com>
2026-03-17 23:16:43 +0900

    refactor: axiosinterceptor.js의 디버그 로그를 제거한다.

--- src/plugins/axiosinterceptor.js
+++ src/plugins/axiosinterceptor.js
@@ -17,21 +17,9 @@ api.interceptors.request.use(
 
 api.interceptors.response.use(
     (response) => {
-        console.log("응답을 받기 전 실행");
         return response;
     },
     (error) => {
-        console.log("응답 받을 때 에러 처리");
-
-        if (error.response) {
-            console.log("에러 상태 코드:", error.response.status);
-            console.log("에러 응답 데이터:", error.response.data);
-        } else if (error.request) {
-            console.log("응답을 받지 못했습니다:", error.request);
-        } else {
-            console.log('요청 설정 에러:', error.message);
-        }
-
         return Promise.reject(error);
     }
 );
